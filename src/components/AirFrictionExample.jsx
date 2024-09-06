09eab6b9a65943c878d7496cfa46180c45e5802e
Uttake <88742186+Uttake@users.noreply.github.com>
2024-09-06 21:37:35 +0300

    some function

--- src/components/AirFrictionExample.jsx
+++ src/components/AirFrictionExample.jsx
@@ -38,13 +38,14 @@ const AirFrictionExample = () => {
       physics: {
         default: "matter",
         matter: {
-          debug: {
-            showAxes: true, // Показывать оси тел
-            showAngleIndicator: true, // Показывать углы
-            showVelocity: true, // Показывать векторы скорости
-            showCollisions: true, // Показывать области столкновений
-            showBounds: true, // Показывать границы тел
-          },
+          gravity: { y: 0.8 },
+          // debug: {
+          //   showAxes: true, // Показывать оси тел
+          //   showAngleIndicator: true, // Показывать углы
+          //   showVelocity: true, // Показывать векторы скорости
+          //   showCollisions: true, // Показывать области столкновений
+          //   showBounds: true, // Показывать границы тел
+          // },
         },
       },
       scene: {
@@ -90,67 +91,87 @@ const AirFrictionExample = () => {
       {
         x: screenWidth * 0.8,
         y: screenHeight * 0.7,
-        invis: true,
+        points: [
+          { x: -30, y: 10 }, // Верхняя точка
+          { x: 30, y: 10 }, // Правая верхняя
+          { x: 30, y: -10 }, // Правая нижняя
+          { x: -30, y: -10 }, // Нижняя точка
+        ],
       },
       {
         x: screenWidth * 0.2,
         y: screenHeight * 0.5,
         reverse: true,
-        invis: true,
+        points: [
+          { x: 30, y: -10 }, // Верхняя точка
+          { x: -30, y: -10 }, // Правая верхняя
+          { x: -30, y: 10 }, // Правая нижняя
+          { x: 30, y: 10 }, // Нижняя точка
+        ],
       },
 
       {
         x: screenWidth * 0.2,
         y: screenHeight * 0.7,
         reverse: true,
-        invis: true,
+        points: [
+          { x: 30, y: -10 }, // Верхняя точка
+          { x: -30, y: -10 }, // Правая верхняя
+          { x: -30, y: 10 }, // Правая нижняя
+          { x: 30, y: 10 }, // Нижняя точка
+        ],
       },
     ];
+
+    let housesCord = [];
 
     house.map((item) => {
       let house = this.matter.add.image(item.x, item.y, "house", {});
 
+      housesCord.push([house.xm]);
+
       house.setStatic(true);
       house.setBody(null);
-      item.reverse ? house.setScale(-0.3, 0.3) : house.setScale(0.3, 0.3);
 
-      if (item.points) {
-        house.setSensor(true);
-        var rect1 = this.matter.add.rectangle(
-          house.x - 40,
-          house.y + 46,
-          75,
-          10,
-          {
-            isStatic: true,
-            angle: Phaser.Math.DegToRad(151),
-          }
-        );
+      house.setSensor(true);
 
-        // Верхний прямоугольник
+      const rect1 = this.matter.add.rectangle(
+        item.reverse ? house.x + 40 : house.x - 40,
+        house.y + 46,
+        75,
+        10,
+        {
+          isStatic: true,
+          angle: Phaser.Math.DegToRad(151),
+        }
+      );
 
-        // Создание второго прямоугольного тела (ниже)
-        var rect2 = this.matter.add.rectangle(
-          house.x + 25,
-          house.y + 28,
-          70,
-          10,
-          {
-            isStatic: true,
-          }
-        ); // Нижний прямоугольник
+      const rect2 = this.matter.add.rectangle(
+        item.reverse ? house.x - 25 : house.x + 25,
 
-        // Соединение прямоугольников с изображением курятника
-        this.matter.add.constraint(house, rect1, 0, 0.9, {
-          pointA: { x: 0, y: -30 },
-          pointB: { x: 0, y: 0 },
-        });
-        this.matter.add.constraint(house, rect2, 0, 0.9, {
-          pointA: { x: 0, y: 30 },
-          pointB: { x: 0, y: 0 },
-        });
-        // Соединяем порог и основной объект в составное тело
+        house.y + 28,
+        70,
+        10,
+        {
+          isStatic: true,
+        }
+      );
+      if (item.reverse) {
+        house.setScale(-0.3, 0.3);
+        Matter.Body.scale(rect1, -1, 1);
+      } else {
+        house.setScale(0.3, 0.3);
       }
+
+      // Соединение прямоугольников с изображением курятника
+      this.matter.add.constraint(house, rect1, 0, 0.9, {
+        pointA: { x: 0, y: -30 },
+        pointB: { x: 0, y: 0 },
+      });
+      this.matter.add.constraint(house, rect2, 0, 0.9, {
+        pointA: { x: 0, y: 30 },
+        pointB: { x: 0, y: 0 },
+      });
 
       if (item.invis) {
         house.setSensor(true);
@@ -159,18 +180,35 @@ const AirFrictionExample = () => {
 
     this.eggs = [];
 
-    for (let i = 0; i < 5; i++) {
+    const eggCount = 4;
+    const minDelay = 500;
+    const maxDelay = 1500;
+
+    for (let i = 0; i < 4; i++) {
       // Создаем физическое тело в виде эллипса (предполагая, что яйцо овальное)
-      let egg = this.matter.add.image(50 + i * 100, 50, "egg", null, {
-        shape: "circle", // Matter.js не поддерживает эллипсы напрямую, поэтому используем круг
-        restitution: 0.8, // Отскок
-      });
+      const delay = Phaser.Math.Between(minDelay, maxDelay);
+      setTimeout(() => {
+        // Позиции для яйца
+        let x = house[i].reverse ? house[i].x + 20 : house[i].x - 20;
+        let y = house[i].reverse ? house[i].y + 20 : house[i].y - 20;
 
-      // Масштабируем изображение яйца
-      egg.setScale(0.1);
+        // Создаем физическое тело в виде эллипса (предполагая, что яйцо овальное)
+        let egg = this.matter.add.image(x, y, "egg", null, {
+          shape: "circle",
+          restitution: 0.6,
+          friction: 0,
+          frictionAir: 0.1,
+          density: 0.001, // Отскок
+        });
 
-      // Добавляем яйцо в массив
-      this.eggs.push(egg);
+        // Масштабируем изображение яйца
+        egg.setScale(0.1);
+
+        // Добавляем яйцо в массив
+        egg.setAngularVelocity(0.1);
+        egg.setOrigin(0.5, 0.5);
+        this.eggs.push(egg);
+      }, delay);
     }
 
     this.matter.world.setBounds(true);
